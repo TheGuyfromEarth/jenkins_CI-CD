13645934d1c9a0a875b8b91a677d70393d02d6ed
TheGuyfromEarth <rishabhgupta01994@gmail.com>
2021-12-04 19:35:45 +0530

    deliberate compilation error

--- src/main/java/com/example/jenkins_CICD/controller/MyController.java
+++ src/main/java/com/example/jenkins_CICD/controller/MyController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyController {
 
     @GetMapping
-    public String test(){
+    public  test(){
         return "test success";
     }
 }
